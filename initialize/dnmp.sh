0c4c0e23f7036070196144121dc1cbbbdc9c0c42
皮泽培 <848733913@qq.com>
2020-07-21 17:52:59 +0800

    测试

--- initialize/dnmp.sh
+++ initialize/dnmp.sh
@@ -2,5 +2,7 @@
 echo "创建目录：/docker/normphp/dnmp/rc"
 sudo mkdir -p /docker/normphp/dnmp/rc
 sudo cp -r ./rc.sh /docker/normphp/dnmp/rc/rc.sh
-echo “设置开机执行sh”
+echo “设置开机执行sudo bash /docker/normphp/dnmp/rc/rc.sh”
 sudo echo "sudo bash /docker/normphp/dnmp/rc/rc.sh" >> /etc/rc.local
+sudo chmod +x  /etc/rc.local
+sudo chmod 755 /etc/rc.local
