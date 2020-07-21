70d9821ce1522b3e4951b914aab7e196c6c11f68
皮泽培 <848733913@qq.com>
2020-07-21 17:55:25 +0800

    修复路径问题

--- initialize/dnmp.sh
+++ initialize/dnmp.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+Path=$(readlink -f "$(dirname "$0")")
 echo "创建目录：/docker/normphp/dnmp/rc"
 sudo mkdir -p /docker/normphp/dnmp/rc
-sudo cp -r ./rc.sh /docker/normphp/dnmp/rc/rc.sh
+`sudo cp -r ${Path}/rc.sh /docker/normphp/dnmp/rc/rc.sh`
 echo “设置开机执行sudo bash /docker/normphp/dnmp/rc/rc.sh”
 sudo echo "sudo bash /docker/normphp/dnmp/rc/rc.sh" >> /etc/rc.local
 sudo chmod +x  /etc/rc.local
