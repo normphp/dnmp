af0971afe16801f6718394abbfc74503b3d122e0
皮泽培 <848733913@qq.com>
2020-07-21 17:46:13 +0800

    开机启动脚本设置

--- initialize/dnmp.sh
+++ initialize/dnmp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "创建目录：/docker/normphp/dnmp/{rc}"
-sudo mkdir -p /docker/normphp/dnmp/{rc}
+echo "创建目录：/docker/normphp/dnmp/rc"
+sudo mkdir -p /docker/normphp/dnmp/rc
 sudo cp -r ./rc.sh /docker/normphp/dnmp/rc/rc.sh
 echo “设置开机执行sh”
 sudo echo "sudo bash /docker/normphp/dnmp/rc/rc.sh" >> /etc/rc.local
