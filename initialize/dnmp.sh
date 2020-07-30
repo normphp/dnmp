70d9e85846d7219951698b3e4e06e2b99a78a2ba
皮泽培 <848733913@qq.com>
2020-07-30 17:03:48 +0800

    修复初始化安装docker不支持centos8的问题

--- initialize/dnmp.sh
+++ initialize/dnmp.sh
@@ -14,7 +14,7 @@ if [ $? -ne 0 ]; then
     # curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun
     docker -v
     if [ $? -ne 0 ]; then
-      dnf install https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.6-3.3.el7.x86_64.rpm
+      dnf install -y https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.6-3.3.el7.x86_64.rpm
       if [ $? -ne 0 ]; then
         echo -e "\033[31m 安装Dockers失败 \033[0m" && exit
       fi
