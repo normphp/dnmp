a47d9426865d7280dc3b13b428cefec73660b348
皮泽培 <848733913@qq.com>
2020-07-30 16:53:50 +0800

    修复初始化安装docker不支持centos8的问题

--- initialize/dnmp.sh
+++ initialize/dnmp.sh
@@ -2,16 +2,24 @@
 # 更新
 cat /etc/redhat-release
 # yum -y update
-yum  -y install vim wget openssl
+yum  -y -q install vim wget openssl
 
 #******************安装decker ******************
 sudo docker -v
 if [ $? -ne 0 ]; then
-    echo '安装docker'
-    curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun \
+    echo '安装docker' \
+    & curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun
     #wget https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.13-3.1.el7.x86_64.rpm \
     #&& sudo yum -y install ./containerd.io-1.2.13-3.1.el7.x86_64.rpm && rm -rf containerd.io-1.2.13-3.1.el7.x86_64.rpm \
     # curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun
+    if [ $? -ne 0 ]; then
+      dnf install https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.6-3.3.el7.x86_64.rpm
+      if [ $? -ne 0 ]; then
+        echo -e "\033[31m 安装Dockers失败 \033[0m" && exit
+      fi
+    else
+        echo -e "\033[32m 安装Docker成功 \033[0m"
+    fi
     echo '启动docker' \
     &&  sudo systemctl start docker \
     &&  echo '设置开机启动' \
