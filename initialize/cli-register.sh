5daeedbe9d9d6d0f1296d21eb09be66093be3f78
皮泽培 <848733913@qq.com>
2020-08-01 17:08:42 +0800

    修复compose模板写入问题

--- initialize/cli-register.sh
+++ initialize/cli-register.sh
@@ -46,7 +46,7 @@ fi
   else
     echo '未注册dnmp快捷命令！现在进行注册'
     sudo echo "alias dnmp='bash ${root_dir}/initialize/dnmp.sh'">>~/.bashrc
-    alias dnmp "bash ${root_dir}/initialize/dnmp.sh"
+    alias dnmp='bash '${root_dir}'/initialize/dnmp.sh'
     source ~/.bashrc
   fi
 }
