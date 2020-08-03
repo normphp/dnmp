1d383c0eea7ce1670812a77f1df4e8a64287986a
皮泽培 <848733913@qq.com>
2020-08-03 09:08:55 +0800

    修改路径

--- initialize/cli-register.sh
+++ initialize/cli-register.sh
@@ -56,7 +56,7 @@ fi
       source ~/.bashrc
   else
     echo '未注册dnmp快捷命令！现在进行注册'
-    sudo echo "alias dnmp='cd ${root_dir}/initialize/ && sudo bash ./dnmp.sh'">>~/.bashrc
+    sudo echo "alias dnmp='sudo bash ${root_dir}/initialize/dnmp.sh'">>~/.bashrc
     # alias dnmp='cd '${root_dir}'/initialize && sudo bash ./dnmp.sh'
     source ~/.bashrc
   fi
