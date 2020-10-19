0065c9e6dcbf402c2f4ecf3b2252b784b0ecd83f
皮泽培 <848733913@qq.com>
2020-10-19 18:12:27 +0800

    修复nginx配置错误

--- initialize/dev-ops-build-function.sh
+++ initialize/dev-ops-build-function.sh
@@ -59,4 +59,28 @@ EOF
   hostname
       #sed -i -e "/${2}/d" /root/.ssh/known_hosts
 }
+devOpsDocument()
+{
+  echo '
+  1、文件结构：
+    1）、nginx配置/docker/normphp/dnmp/data/nginx/conf/default_devops.conf
+    2）、项目代码目录/docker/normphp/dnmp/data/devops/code/devops-admin/[resource为前端资源,normphp为php项目资源]
+    3）、使用独立MySQL、Redis时数据目录/docker/normphp/dnmp/data/devops/data/
+  2、Mysql相关：
+    1）、ROOT密码：默认使用dnmp-master/build/docker-compose/.env文件（注意.env为隐藏文件可以使用ls -l 查看隐藏文件）的MYSQL_ROOT_PASSWORD变量控制（只有第一次执行启动命令时从会有效）
+    2）、默认会创建一个normphp账号 密码为normphp 所有ip可访问
+      a）、默认创建账号为.env 中的MYSQL_USER变量定义 密码为MYSQL_PASSWORD变量定义
+      b）、如需要修改控制ip访问可执行如下命令
+        进入容器 连接mysql  根据提示输入root密码
+        docker exec -it docker-compose_devops-mysql_1  mysql -u root -p
+        选择数据库 mysql
+        use mysql
+        select user, host from user;
+        update user set host = '%' where user = 'root' and host = '%';
+  ';
+
+
+
+
+}
 
