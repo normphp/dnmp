bb52f5de8c0fd15e7aad1158047b79b0edb4a2c4
pizepei <pizepei@pizepei.com>
2020-12-28 16:42:39 +0800

    测试增加composer命令

--- initialize/dnmp-composer.sh
+++ initialize/dnmp-composer.sh
@@ -12,8 +12,11 @@ composer(){
   setPHPComposerBuildYML $composeFile $*
   echo $composeFile
   dockerComposeCli="docker-compose -f ${composeFile} run --rm -w  /data/build/${3} php-fpm-${2}-build  composer ${4} ${5} ${6} ${7} ${8} ${9}"
+  dockerComposeVCli="docker-compose -f ${composeFile} run --rm -w  /data/build/  php-fpm-${2}-build  composer -V"
+  $dockerComposeVCli
   echo $dockerComposeCli
-  ##run --rm -w /root/  php-fpm-7.3-build pwd
+  echo "Local run path /docker/normphp/dnmp/data/build/${3}"
+  echo "***************************************************"
   $dockerComposeCli
 
 }
@@ -31,20 +34,35 @@ setPHPComposerBuildYML(){
     echo '只支持x86_64、aarch64'
     exit
   fi
+  sudo mkdir -p ${root_dir}/build/docker-compose/php/${3}
+
   phpversion="php-fpm-"${3}
-  # 写入配置文件
+
+  # 写入Dockerfile
+echo 'FROM normphp/dnmp-php:"'${phpversion}'-full'${archInfo}'"
+MAINTAINER pizepei "pizepei@pizepei.com"
+#更新安装依赖包和PHP核心拓展
+RUN apt-get update  \
+&& apt-get -y install git
+WORKDIR /data
+# Write Permission
+RUN usermod -u 1000 www-data' > ${root_dir}/build/docker-compose/php/${3}/Dockerfile
+  # 写入compose配置文件
+  # ssh 有安全隐患（暂时先用构建机器的配置）
 echo "version: '3.3'
 services:
   ${phpversion}-build:
     env_file:
     - .env
-    image: normphp/dnmp-php:"${phpversion}-full${archInfo}"
+    build: ./php/${3}
+    #image: normphp/dnmp-php:"${phpversion}-full${archInfo}"
     #    ports:
     #  - "9000:9000"
     networks:
      - dnmpNat
     volumes:
       - /docker/normphp/dnmp/data/build/:/data/build/:rw
+      - ~/.ssh/:/root/.ssh/:ro
       - /docker/normphp/dnmp/data/composer/composer.phar:/usr/local/bin/composer:rw
       - /docker/normphp/dnmp/data/php/etc/"${phpversion}"/php-fpm.conf:/usr/local/etc/php-fpm.conf:ro
       - /docker/normphp/dnmp/data/php/etc/"${phpversion}"/php.ini-development:/usr/local/etc/php/php.ini:ro
